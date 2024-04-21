decb4ab4d1783f822bb5247fa818985b035e8c41
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2024-04-21 20:29:15 +0700

    Update profile-admin.blade.php

--- dukcapilLagi/resources/views/auth/profile-admin.blade.php
+++ dukcapilLagi/resources/views/auth/profile-admin.blade.php
@@ -27,7 +27,12 @@
         <div class="container">
             <div class="flex flex-col">
                 <div class="py-6">
-                    <h2 class="text-2xl font-bold font-monserrat ">Admin Information</h2>
+                    <h2 class="pb-1 text-2xl font-bold font-monserrat">Admin Information</h2>
+                    <a href="/" class="flex items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
+                            viewBox="0 0 512 512">
+                            <path fill="none" stroke="gray" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="36" d="M328 112L184 256l144 144" />
+                        </svg><span class="text-base font-semibold text-secondary_teks">Back home</span></a>
                 </div>
                 <div class="flex flex-col mb-6 gap-14">
                     <div
@@ -50,17 +55,17 @@
                     </div>
                     <div
                         class="flex flex-col p-6 md:w-[800px] bg-background_light rounded-2xl shadow-lg shadow-gray-300/20">
-                        <h3 class="text-lg font-semibold font-nunito">Profil Information</h3>
-                        <p class="text-base text-secondary_teks">Update yout account’s profile information and email
-                            address</p>
+                        <h3 class="text-lg font-semibold font-nunito">Update Password</h3>
+                        <p class="text-base text-secondary_teks">Ensure your account is using a long random password to
+                            stay secure</p>
                         <div class="py-6">
                             <form action="" class="relative flex flex-col w-full max-w-sm gap-4">
-                                <input type="username" name="username" id="username" placeholder="ex@gmail.com"
+                                <input type="currentpw" name="currentpw" id="currentpw" placeholder="Current Password"
                                     class="px-4 py-2 border-2 border-gray-400 rounded-md focus:outline-none">
-                                <input type="email" name="email" id="email" placeholder="ex@gmail.com"
+                                <input type="newpw" name="newpw" id="newpw" placeholder="New Password"
                                     class="px-4 py-2 border-2 border-gray-400 rounded-md focus:outline-none">
-                                <input type="file" name="file" id="file"
-                                    class="text-gray-600 file:absolute file:right-0 file:bg-primary file:text-white file:border-0 file:py-1 file:px-3 file:rounded-lg file:shadow-xl file:shadow-primary/30">
+                                <input type="confirmpw" name="confirmpw" id="confirmpw" placeholder="Confirm Password"
+                                    class="px-4 py-2 border-2 border-gray-400 rounded-md focus:outline-none">
                                 <button type="submit"
                                     class="w-24 px-4 py-2 text-base font-bold rounded-lg bg-primary text-background_light">Save</button>
                             </form>
@@ -68,19 +73,20 @@
                     </div>
                     <div
                         class="flex flex-col p-6 md:w-[800px] bg-background_light rounded-2xl shadow-lg shadow-gray-300/20">
-                        <h3 class="text-lg font-semibold font-nunito">Profil Information</h3>
-                        <p class="text-base text-secondary_teks">Update yout account’s profile information and email
-                            address</p>
+                        <h3 class="text-lg font-semibold font-nunito">Delete Account</h3>
+                        <p class="text-base text-secondary_teks">Once your account is deleted all of its resource and
+                            data will be permanently deleted.
+                            Before deleting your account please download any data or information that you wish to
+                            retain.</p>
                         <div class="py-6">
                             <form action="" class="relative flex flex-col w-full max-w-sm gap-4">
                                 <input type="username" name="username" id="username" placeholder="ex@gmail.com"
                                     class="px-4 py-2 border-2 border-gray-400 rounded-md focus:outline-none">
                                 <input type="email" name="email" id="email" placeholder="ex@gmail.com"
                                     class="px-4 py-2 border-2 border-gray-400 rounded-md focus:outline-none">
-                                <input type="file" name="file" id="file"
-                                    class="text-gray-600 file:absolute file:right-0 file:bg-primary file:text-white file:border-0 file:py-1 file:px-3 file:rounded-lg file:shadow-xl file:shadow-primary/30">
                                 <button type="submit"
-                                    class="w-24 px-4 py-2 text-base font-bold rounded-lg bg-primary text-background_light">Save</button>
+                                    class="w-40 px-4 py-2 text-base font-bold rounded-lg bg-primary text-background_light">Delete
+                                    Account</button>
                             </form>
                         </div>
                     </div>
