1e59116394196631b76bd0faabd1917cdfcfe543
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-04-23 14:10:02 +0700

    firstii

--- routes/web.php
+++ routes/web.php
@@ -39,39 +39,3 @@ Route::delete('/mitra-{id}', [MitraController::class, 'destroy'])->name('mitra.d
 //Home
 Route::get('/', [HomeController::class, 'index']);
 
-
-Route::get('/form-berita', function () {
-    return view('admin.berita.form-berita');
-});
-
-Route::get('/profile-admin', function () {
-    return view('auth.profile-admin');
-});
-
-Route::get('/download', function () {
-    return view('download');
-});
-Route::get('/profile', function () {
-    return view('profil-section.profile');
-});
-Route::get('/tupoksi-visi-misi', function () {
-    return view('profil-section.tupoksiVM');
-});
-Route::get('/struktur-organisasi', function () {
-    return view('profil-section.strukturOrg');
-});
-Route::get('/kontak', function () {
-    return view('profil-section.kontak');
-});
-Route::get('/persyaratan', function () {
-    return view('standar-section.persyaratan');
-});
-Route::get('/detail-persyaratan', function () {
-    return view('standar-section.detailPersyaratan');
-});
-Route::get('/detail-download', function () {
-    return view('standar-section.detailDownload');
-});
-Route::get('/faq', function () {
-    return view('faq');
-});
