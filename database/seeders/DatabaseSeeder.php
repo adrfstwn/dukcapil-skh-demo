74870804eba81df273acc2b559501fbcf72026be
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-05-01 13:14:22 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Test Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('adminskh'), // Anda dapat menggunakan Hash untuk mengenkripsi password
         ]);
     }
 }
