9f2b797177fcedf8778e14636db09575e5668cac
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2024-06-04 22:29:09 +0700

    Update index.blade.php

--- resources/views/admin/berita/index.blade.php
+++ resources/views/admin/berita/index.blade.php
@@ -1,11 +1,58 @@
 @extends('master-admin')
 @section('content')
-    <section id="detailpersyaratan-index">
+    <section id="Berita">
         <div class="container">
             <div class="my-6">
-                <button type="button" 
+                <button type="button"
                     class="text-white bg-red-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                    Add Berita 
+                    Add Kategori Berita
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                </button>
+            </div>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Kategori Berita
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                            </td>
+                            <td class="flex items-center px-6 py-4">
+                                <a href=""
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                <form action="" method="POST" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</button>
+                                </form>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <section id="Berita">
+        <div class="container">
+            <div class="my-6">
+                <button type="button"
+                    class="text-white bg-red-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                    Add Berita
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -37,45 +84,42 @@
                         </tr>
                     </thead>
                     <tbody>
-                      
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="w-4 p-4">
-                                </td>
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    
-                                </th>
-                                <td class="px-6 py-4">
-                                   
-                                </td>
-                                <td class="px-6 py-4">
-                                   
-                                </td>
-                                <td class="px-6 py-4">
-                                    <img src=""
-                                        alt="Detail Persyaratan Image" class="w-70">
-                                </td>
-                                <td class="flex items-center px-6 py-4">
-                                    <a href=""
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <form
-                                        action=""
-                                        method="POST" class="inline-block">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                            class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</button>
-                                    </form>
-                                </td>
-                            </tr>
-        
+
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                            </td>
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+
+                            </th>
+                            <td class="px-6 py-4">
+
+                            </td>
+                            <td class="px-6 py-4">
+
+                            </td>
+                            <td class="px-6 py-4">
+                                <img src="" alt="Detail Persyaratan Image" class="w-70">
+                            </td>
+                            <td class="flex items-center px-6 py-4">
+                                <a href=""
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                <form action="" method="POST" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</button>
+                                </form>
+                            </td>
+                        </tr>
+
                     </tbody>
                 </table>
             </div>
         </div>
     </section>
-    <section id="berita">
+    <section id="berita-card">
         <div class="container">
             <div class="grid md:grid-cols-4 gap-4 justify-between my-10">
                 <div
