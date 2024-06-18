5dabd5bd8c4ab7d1cf565f8fa81750b738ed0146
yudhaperwira17 <116625101+yudhaperwira17@users.noreply.github.com>
2024-06-18 21:25:04 +0700

    menu dinamis

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,7 +54,8 @@
                           </div>
                       </div>
                   </div>
-                  <div @click.away="open = false" class="relative" x-data="{ open: false }">
+
+                  <!-- <div @click.away="open = false" class="relative" x-data="{ open: false }">
                       <button @click="open = !open"
                           class="flex flex-row items-center w-full px-2 py-2 mt-2 text-sm font-bold text-left bg-transparent rounded-lg style-menu-navbar md:w-auto md:inline md:mt-0 md:ml-3 focus:outline-none focus:shadow-outline">
                           <span>STANDAR PELAYANAN</span> <svg fill="currentColor" viewBox="0 0 20 20"
@@ -145,8 +146,8 @@
                               </div>
                           </div>
                       </div>
-                  </div>
-                  <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                  </div> -->
+                  <!-- <div @click.away="open = false" class="relative" x-data="{ open: false }">
                       <button @click="open = !open"
                           class="flex flex-row items-center w-full px-2 py-2 mt-2 text-sm font-bold text-left bg-transparent rounded-lg  style-menu-navbar md:w-auto md:inline md:mt-0 md:ml-3 focus:outline-none focus:shadow-outline">
                           <span>DATA</span>
@@ -226,8 +227,8 @@
                               </div>
                           </div>
                       </div>
-                  </div>
-                  <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                  </div> -->
+                  <!-- <div @click.away="open = false" class="relative" x-data="{ open: false }">
                       <button @click="open = !open"
                           class="flex flex-row items-center w-full px-2 py-2 mt-2 text-sm font-bold text-left bg-transparent rounded-lg  style-menu-navbar md:w-auto md:inline md:mt-0 md:ml-3 focus:outline-none focus:shadow-outline">
                           <span>AKUNTABILITAS KINERJA</span>
@@ -288,7 +289,7 @@
                               </div>
                           </div>
                       </div>
-                  </div>
+                  </div> -->
                   @foreach ($menu as $menu)
                       @if ($menu->kontenMenus && $menu->kontenMenus->isNotEmpty())
                           <!-- Menu with content directly links to content -->
@@ -363,40 +364,9 @@
                       @endif
                   @endforeach
                   <a class="px-2 py-2 mt-2 text-sm font-bold font-nunito  rounded-lg  md:mt-0 md:ml-3 style-menu-navbar focus:outline-none focus:shadow-outline"
+                      href="persyaratan">PERSYARATAN</a>
+                  <a class="px-2 py-2 mt-2 text-sm font-bold font-nunito  rounded-lg  md:mt-0 md:ml-3 style-menu-navbar focus:outline-none focus:shadow-outline"
                       href="{{ route('download.tampil') }}">DOWNLOAD</a>
-                  <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                      <button @click="open = !open"
-                          class="flex flex-row items-center w-full px-2 py-2 mt-2 text-sm font-bold text-left bg-transparent rounded-lg  style-menu-navbar md:w-auto md:inline md:mt-0 md:ml-3 focus:outline-none focus:shadow-outline">
-                          <span>PPID</span>
-                          <svg fill="currentColor" viewBox="0 0 20 20"
-                              :class="{ 'rotate-180': open, 'rotate-0': !open }"
-                              class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-                              <path fill-rule="evenodd"
-                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                  clip-rule="evenodd"></path>
-                          </svg>
-                      </button>
-                      <div x-show="open" x-transition:enter="transition ease-out duration-100"
-                          x-transition:enter-start="transform opacity-0 scale-95"
-                          x-transition:enter-end="transform opacity-100 scale-100"
-                          x-transition:leave="transition ease-in duration-75"
-                          x-transition:leave-start="transform opacity-100 scale-100"
-                          x-transition:leave-end="transform opacity-0 scale-95"
-                          class="absolute z-10 right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                          <div class="px-2 py-2 bg-background_light rounded-md shadow ">
-                              <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                  href="#">WAJIB DIUMUMKAN <br> SECARA BERKALA</a>
-                              <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                  href="#">WAJIB DIUMUMKAN <br> SERTA MERTA</a>
-                              <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                  href="#">WAJIB DIUMUMKAN <br> SETIAP SAAT </a>
-                              <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                  href="#">DAFTAR INFORMASI <br> YANG DIKECUALIKAN</a>
-
-                          </div>
-                      </div>
-                  </div>
-
               </nav>
           </div>
       </div>
