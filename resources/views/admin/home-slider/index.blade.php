b55cc4493e62729678ad98ef1e13307a6385c106
adrfstwn <100577989+adrfstwn@users.noreply.github.com>
2024-06-26 19:17:21 +0700

    membetulkan home

--- resources/views/admin/home-slider/index.blade.php
+++ resources/views/admin/home-slider/index.blade.php
@@ -38,7 +38,7 @@
                     @foreach($homeSliders as $homeSlider)
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                             id="homeSlider{{ $homeSlider->id }}">
-                            >
+
                             <td class="w-4 p-4">
                             </td>
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
